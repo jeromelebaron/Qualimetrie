05a0dfb5e3f901d6a3a63be865e1392180bcc50f
Jerdopler <lebaronjerome@free.fr>
2016-05-02 09:42:56 +0200

    Ajout de l'exception

---
+++ Qualimetrie-Entity/src/main/java/fr/afcepf/atod26/qualimetrie/exception/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package contenant les exception.
+ * @author Jérome LE BARON
+ * @author $LastChangedBy$
+ * @version $Revision$ $Date$
+ */
+package fr.afcepf.atod26.qualimetrie.exception;
+
