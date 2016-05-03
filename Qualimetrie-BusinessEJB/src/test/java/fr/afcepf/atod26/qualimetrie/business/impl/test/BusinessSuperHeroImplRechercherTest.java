fcb9d6f122e058900419decb57193f6b2a6f7bd9
Jerdopler <lebaronjerome@free.fr>
2016-05-03 12:02:14 +0200

    Configuration spécifique à checkstyle Ajout dans le pom des éléments pour générer la javadoc

---
+++ Qualimetrie-BusinessEJB/src/test/java/fr/afcepf/atod26/qualimetrie/business/impl/test/BusinessSuperHeroImplRechercherTest.java
@@ -0,0 +1,28 @@
+/**
+ * Créé le 3 mai 2016 par Jérome LE BARON
+ */
+package fr.afcepf.atod26.qualimetrie.business.impl.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import fr.afcepf.atod26.qualimetrie.entity.SuperHero;
+
+/**
+ * Tests pour la méthode de récupération de tous les {@link SuperHero}.
+ * @author Jérome LE BARON
+ * @author $LastChangedBy$
+ * @version $Revision$ $Date$
+ */
+public class BusinessSuperHeroImplRechercherTest {
+
+    /**
+     * La méthode de test pour récupérer tous les {@link SuperHero}.
+     */
+    @Test
+    public void testRecupererTousLesSuperHero() {
+        fail("Not yet implemented");
+    }
+
+}
