30689f6e71588b4ce5405ebbcb665f26f149ec3b
Jerdopler <lebaronjerome@free.fr>
2016-05-04 15:12:29 +0200

    Ajout des imports hibernate

---
+++ Qualimetrie-Entity/src/test/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO `super_hero` (`id_super_hero`,`nom`,`prenom`,`super_nom`) VALUES (1,'Parker', 'Peter', 'Spiderman');
+INSERT INTO `super_hero` (`id_super_hero`,`nom`,`prenom`,`super_nom`) VALUES (2,'Wayne', 'Bruce', 'Batman');
+INSERT INTO `super_hero` (`id_super_hero`,`nom`,`prenom`,`super_nom`) VALUES (3,'Odinson', 'Thor', 'Thor');
+INSERT INTO `super_hero` (`id_super_hero`,`nom`,`prenom`,`super_nom`) VALUES (4,'Howlett', 'James', 'Wolverine');
+
+INSERT INTO `costume` (`id_costume`,`couleur`,`is_moulant`,`nom`,`id_super_hero`) VALUES (1, 'noire', false, 'BatCape', 2);
+INSERT INTO `costume` (`id_costume`,`couleur`,`is_moulant`,`nom`,`id_super_hero`) VALUES (2, 'rouge', true, 'SpiderCostume', 1);
+INSERT INTO `costume` (`id_costume`,`couleur`,`is_moulant`,`nom`,`id_super_hero`) VALUES (1, 'bleu' ,true, 'XTenue', 4);
